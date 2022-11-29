771a7a30d376ae4a7f5d26ada4c8a93bf3e12218
miguelbinue <miguelbprada@gmail.com>
2022-11-29 13:16:19 +0100

    Done assignment w3d1

---
+++ LAB/vue/lab-vue-setup-main/lab-vue-setup/src/components/newComponents/NewComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- <h1>{{ saludo }}</h1> -->
+  <button @click="hello">{{ saludo }}</button>
+  
+</template>
+
+<script setup>
+//el export default no puede ir dentro del mismo script junto a "<script steup>"
+const saludo = "Hola soy Miguel";
+function hello() {
+    console.log(saludo + " en consola");
+}
+
+// export default {
+// }
+</script>
+
+
+<style>
+
+</style>
